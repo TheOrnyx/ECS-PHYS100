1c28ac3ec09dc426083caf5348c5655d89c0bc1a
MrAlcoholicFox <cthulhu345@gmail.com>
2023-05-03 09:04:53 +1200

    Object collisions now work, still need to fix the jitteryness tho

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import ecs100.UI;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -11,9 +9,7 @@ public class Main {
     public static effector colliisionObj = new effector(500);
     private static Main main = new Main();
     private static mouseListener mouse = new mouseListener();
-
-
-    static final long tickRateMS = 10;
+    static ArrayList<fallObject> physObjects = new ArrayList<fallObject>();
     public void setupGUI(){
         UI.initialise();
         UI.setWindowSize(1000, 800);
@@ -22,12 +18,16 @@ public class Main {
     }
 
     public void start(double x, double y){
+        physObjects.add(new fallObject(x, y, colliisionObj, physObjects));
+    }
+
+    /*public void start(double x, double y){
         fallObject johnathan = new fallObject(x, y, colliisionObj);
         final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(johnathan::startPhys, 0, 10, TimeUnit.MILLISECONDS);
         johnathan.draw();
         johnathan.startPhys();
-    }
+    }*/
     public static void main(String[] args) {
         mouse.mouseInit(colliisionObj, main);
         main.setupGUI();
--- src/fallObject.java
+++ src/fallObject.java
@@ -1,22 +1,36 @@
 import ecs100.UI;
 
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
 public class fallObject {
     private double X;
     private double Y;
     private final double squareSize = 40;
-    static final double tickRate = Main.tickRateMS;
+    /*static final double tickRate = Main.tickRateMS;
     final double ticksPerSecond = (double)1000/Main.tickRateMS;
-    static final double gravity_Sec = 196;
+    static final double gravity_Sec = 196;*/
     static final double gravityTick = 0.196;
     private double acceleration = gravityTick;
 
     private double velocity = 0;
     private effector collider;
 
-    public fallObject(double initX, double initY, effector collider){
+    private ArrayList<fallObject> objectsVec;
+    private double index;
+
+    public fallObject(double initX, double initY, effector collider, ArrayList<fallObject> objectsVec){
         this.X = initX;
         this.Y = initY;
         this.collider = collider;
+        this.objectsVec = objectsVec;
+        this.index = this.objectsVec.indexOf(this);
+        this.draw();
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(this::startPhys, 0, 10, TimeUnit.MILLISECONDS);
     }
 
     public void draw(){
@@ -24,24 +38,83 @@ public class fallObject {
     }
 
     public void startPhys(){
-        if (collisionCheck()) {
-            UI.eraseRect(X, Y, squareSize, squareSize);
-            this.velocity = velocity+acceleration;
-            this.Y = this.Y + velocity;
-            this.draw();
-            collider.draw();
+        boolean checked = objectCollision();
+        if (!checked) {
+            if (collisionCheck()) {
+                UI.eraseRect(X, Y, squareSize, squareSize);
+                this.velocity = velocity + acceleration;
+                this.Y = this.Y + velocity;
+                this.draw();
+                collider.draw();
+
+            } else {
+                UI.eraseOval(X, Y, squareSize, squareSize);
+                this.Y = collider.getY() - squareSize;
+                this.velocity = (this.velocity * -1) + this.velocity * 0.1;
+                this.draw();
+            }
         }
-        else{
-            UI.eraseOval(X, Y, squareSize, squareSize);
-            this.Y = collider.getY()-squareSize;
-            this.velocity = (this.velocity*-1)+this.velocity*0.1;
-            this.draw();
+        /*else {
+            this.Y -= 0.5;
+        }*/
+        UI.eraseRect(X, Y, squareSize, squareSize);
+        this.draw();
+        UI.println(this.velocity);
+    }
+
+    public boolean objectCollision(){
+        for (fallObject obj: objectsVec){
+            if (objectsVec.indexOf(obj) != objectsVec.indexOf(this)){
+                double distance = distance_check(this.X, this.Y, obj.getX(), obj.getY());
+                if (distance <= squareSize){
+                    if (this.Y <= obj.getY() && !(Math.abs(obj.getVelocity()) < 1)){
+                        this.setVelocity(Math.abs(obj.getVelocity())*-1);
+                        obj.setVelocity(0);
+                        UI.eraseRect(X, Y, squareSize, squareSize);
+                        this.Y = obj.getY()-squareSize;
+                        return true;
+                    }
+                    else if (this.Y <= obj.getY() && Math.abs(obj.getVelocity()) < 1){
+                        UI.eraseRect(X, Y, squareSize, squareSize);
+                        this.Y = obj.getY() - squareSize;
+                        this.velocity = (this.velocity * -1) + this.velocity * 0.1;
+                        return true;
+                    }
+                    /*else if (this.Y >= obj.getY()){
+                        obj.setVelocity(-1*this.getVelocity());
+                    }*/
+                }
+
+            }
         }
+        return false;
     }
 
     public boolean collisionCheck(){
         return this.Y + velocity+squareSize <= collider.getY();
     }
 
+    public double distance_check(double x1, double y1, double x2, double y2){
+        return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
+    }
 
+    public double getX() {
+        return X;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public void setY(double y) {
+        Y = y;
+    }
 }
